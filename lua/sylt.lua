6dbac94a6ca3acdfb0ac5ca1f305544915b8820f
Empa <emanuel@empa.xyz>
2023-02-22 02:31:21 +0100

    a

--- lua/sylt.lua
+++ lua/sylt.lua
@@ -4,7 +4,7 @@ function M.setup_treesitter()
 	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 	parser_config.sylt = {
 		install_info = {
-			url = "git@github.com:Quaqqer/nvim-treesitter-sylt",
+			url = "git@github.com:Quaqqer/nvim-treesitter-sylt.git",
 			files = { "src/parser.c" },
 		},
 		filetype = "sylt",
